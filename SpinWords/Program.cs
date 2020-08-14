c720968c6723a5cba5446d317f307887a63a41d7
Tim May <timmay1123@gmail.com>
2020-08-14 22:14:59 +1200

    Init

---
+++ SpinWords/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SpinWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Sentence:");
+
+            var sentence = Console.ReadLine();
+            Console.WriteLine(SpinWords(sentence));
+            Console.ReadLine();
+        }
+
+        public static string SpinWords(string sentence)
+        {
+            string x = string.Empty;
+
+            var parts = sentence.Split(' ');
+            foreach (var part in parts)
+            {
+                var y = string.Empty;
+                if (part.Length > 4)
+                {
+                    y = ReverseWord(part) + " ";
+                }
+                else
+                {
+                    y = part + " ";
+                }
+                x += y;
+            }
+            return x.Trim(' ');
+        }
+
+        public static string ReverseWord(string word)
+        {
+            char[] letters = word.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+    }
+}
